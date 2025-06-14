71f693607ba5dc63c0edf71c8a9874f354befbfb
Deeba132 <deebashabreen@gmail.com>
2025-06-15 03:20:00 +0530

    added files

--- wild-oasis/src/services/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://cgdjsxbbgrajybjktrkd.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnZGpzeGJiZ3Jhanliamt0cmtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1Mjg5OTIsImV4cCI6MjA2NTEwNDk5Mn0.ONenmldZYhx4QTN0cOcQEw1NghegqQjgiFEJdiNrycI";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
