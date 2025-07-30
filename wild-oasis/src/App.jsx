8ea4ba7a87dc6a95f30bdabd1d66117893dce1bc
Deeba132 <deebashabreen@gmail.com>
2025-07-30 18:25:34 +0530

    added

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -17,6 +17,7 @@ import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import ProtectedRoute from "./ui/ProtectedRoute";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,6 +29,9 @@ const queryClient = new QueryClient({
 });
 
 function App() {
+  useEffect(() => {
+    toast.success("Just click on login button to login");
+  }, []);
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
