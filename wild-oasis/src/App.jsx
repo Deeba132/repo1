cf0cf6bca9b60536d2e7b1e56450fb8dac996492
Deeba132 <deebashabreen@gmail.com>
2025-07-30 18:29:35 +0530

    Update App.jsx

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
+import { Toaster,toast} from "react-hot-toast";
 import {useEffect} from "react";
 
 import GlobalStyles from "./styles/GlobalStyles";
