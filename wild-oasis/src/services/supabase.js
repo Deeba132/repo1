d128a1df17085ba056b586d23ac80d51f62558d0
Deeba132 <deebashabreen@gmail.com>
2025-12-01 13:41:43 +0530

    changes made

--- wild-oasis/src/services/supabase.js
+++ wild-oasis/src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://cgdjsxbbgrajybjktrkd.supabase.co";
+export const supabaseUrl = "https://pznxrdmibklhwjbunqji.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnZGpzeGJiZ3Jhanliamt0cmtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1Mjg5OTIsImV4cCI6MjA2NTEwNDk5Mn0.ONenmldZYhx4QTN0cOcQEw1NghegqQjgiFEJdiNrycI";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6bnhyZG1pYmtsaHdqYnVucWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1OTIzNjQsImV4cCI6MjA3ODE2ODM2NH0.4CBuLUJuSli-m7CRA8JKa2JdC_whvOag6xMLCpj-SQM";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
